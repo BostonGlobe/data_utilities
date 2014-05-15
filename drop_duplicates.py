40b37006c7113c8eedc9928c5296621ceedd4f44
Gabriel Florit <gabrielflorit@gmail.com>
2014-05-15 19:22:07 -0400

    Update drop_duplicates.py

--- drop_duplicates.py
+++ drop_duplicates.py
@@ -3,7 +3,6 @@ import argparse
 import sys
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--csv', help='the csv file to process, otherwise reads from stdin')
 parser.add_argument('--column', help='the column to remove duplicates by')	
 args = parser.parse_args()
 
@@ -11,10 +10,6 @@ if args.column is None:
 	print '--column option is required'
 	sys.exit(0)
 
-if args.csv is None:
-	df = pd.read_csv(sys.stdin)
-else:
-	df = pd.read_csv(args.csv)
-
-df2=df.drop_duplicates(cols=args.column, take_last=True)
+df = pd.read_csv(sys.stdin)
+df2 = df.drop_duplicates(cols=args.column, take_last=True)
 df2.to_csv(sys.stdout, index=False)
